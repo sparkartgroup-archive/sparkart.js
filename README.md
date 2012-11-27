c4ffb0702fc8fd5bb8817daa345ef47fd68e7f9c
Fauntleroy <the.kaempfer@gmail.com>
2012-11-26 16:38:36 -0800

    updated readme
    changed methods to prototype methods
    added more templates

--- README.md
+++ README.md
@@ -1,4 +1,148 @@
-sparkart.js
-===========
+# sparkart.js
 
 Easily interact with Sparkart's APIs via Javascript.
+
+## Usage
+
+In order to use sparkart.js you just need to include the sparkart.js file:
+
+```html
+<script src="sparkart.js"></script>
+```
+
+Including this file makes the `sparkart` variable available. A multitude of utilities can then be used:
+
+### Fanclubs
+
+Sparkart.js's `Fanclub` utility enables you to access information about a fanclub and include various fanclub widgets in your website. Every fanclub has a unique API key which grants access to that fanclub's data. To work with a specific fanclub, just start a new instance of `sparkart.Fanclub` using the API key for that fanclub.
+
+```javascript
+var bonjovi = new sparkart.Fanclub(API_KEY); // This creates an instance of sparkart.Fanclub
+```
+
+Now you can utilize your new fanclub object:
+
+```javascript
+bonjovi.get( 'plans', function( err, plans ){
+	if( err ) console.log( err ); // err is null if nothing goes wrong
+	else console.log( plans );
+});
+bonjovi.is( 'logged in', function( err, logged_in ){
+	if( err ) console.log( err ); // err is null if nothing goes wrong
+	else console.log( logged_in );
+});
+bonjovi.draw();
+```
+
+#### Fanclub Widgets
+
+In order to make generating fanclub markup easier, sparkart.js has a widget system which automatically renders fanclub html. The following widgets are available by default:
+
+- **account** - The currently logged in user's information.
+- **login** - A login form for the fan club.
+- **register** - A registration form for the fan club.
+- **subscriptions** - The user's current subscriptions.
+- **subscription** - A single subscription.
+- **plans** - The plans available for this fan club.
+- **plan** - A single plan.
+- **events** - A list of events.
+- **event** - A single event.
+- **orders** - A list of orders.
+- **order** - A single order.
+
+To use a widget, you need to create a container for that widget with the classes `sparkart` `fanclub` and the widget's name (ex: `account`). Then create the fanclub object or run the `draw()` method if the fanclub object has already been instantiated:
+
+```html
+<!-- fanclub object does not exist -->
+<div class="sparkart fanclub account"></div>
+<script>
+	var bonjovi = new sparkart.Fanclub(API_KEY);
+</script>
+
+<!-- fanclub object already exists -->
+<div class="sparkart fanclub subscriptions"></div>
+<script>
+	bonjovi.draw();
+</script>
+```
+
+Custom templates can be defined for fanclub widgets when their markup is not ideal. These templates are all written in [Handlebars](http://handlebarsjs.com/), and they draw their information from the [Sparkart Fanclubs API](http://fanclubs.sparkart.com/developers). New templates must be passed as strings when initializing the fanclub object:
+
+```javascript
+var alternate_template = '<div class="account"><h3>{{username}}</h3><span class="email">{{email}}</span></div>';
+var bonjovi = new sparkart.Fanclub( API_KEY, {
+	templates: {
+		account: alternate_template
+	}
+});
+```
+
+#### Fanclub Methods
+
+##### .logout( callback )
+
+- **callback** - A function to be executed after the logout method completes. Gets `err`.
+
+Logs a user out.
+
+```javascript
+bonjovi.logout( function( err ){
+	if( err ) return err;
+	console.log('User successfully logged out!');
+});
+```
+
+##### .draw( widget, config )
+
+- **widget** - A jQuery selector (as a string) or a jQuery selection of a specific widget to draw. If nothing is specified, it draws all widgets on the page.
+- **config** - An object of options to pass when rendering the widget. These options are listed on the endpoints for the [Sparkart Fanclubs API](http://fanclubs.sparkart.com/developers).
+
+Draws a single widget, collection of widgets, or every widget on the page.
+
+```javascript
+bonjovi.draw(); // draws/redraws every widget
+bonjovi.draw( '#events', {
+	start: 5
+}); // draws/redraws the widget at '#events'
+```
+
+##### .get( endpoint, parameters, callback )
+
+- **endpoint** - An API endpoint to get information from. ('logout','account','subscriptions','plans','plans/5','events','orders')
+- **parameters** - An object of options for the request. These options are listed on the endpoints for the [Sparkart Fanclubs API](http://fanclubs.sparkart.com/developers)
+- **callback** - A function to be executed after the request finishes
+
+Gets information from the Fanclubs API.
+
+```javascript
+bonjovi.get( 'account', function( err, user ){
+	if( err ) return err;
+	console.log( 'The user\'s account information', user );
+});
+```
+
+##### .post( endpoint, parameters, callback )
+
+- **endpoint** - An API endpoint to get information from. ('login','register')
+- **parameters** - An object of options for the request. These options are listed on the endpoints for the [Sparkart Fanclubs API](http://fanclubs.sparkart.com/developers)
+- **callback** - A function to be executed after the request finishes
+
+Posts information to the Fanclubs API.
+
+```javascript
+bonjovi.post( 'login', {
+	username: 'BJFan68',
+	password: '1234'
+}, function( err, user ){
+	if( err ) return err;
+	console.log( 'User has logged in!', user );
+});
+```
+
+##### .destroy()
+
+Destroy all markup and bindings created by the fanclub.
+
+```javascript
+bonjovi.destroy(); // all markup reverts to pre-fanclub state
+```
--- sparkart.js
+++ sparkart.js
@@ -12,13 +12,17 @@ this.sparkart = {};
 		var fanclub = this;
 		var templates = {
 			subscriptions: '<ul class="subscriptions">{{#subscriptions}}<li>{{name}}</li>{{/subscriptions}}</ul>',
+			subscription: '<div class="subscription">{{name}}</div>',
 			plans: '<ul class="plans">{{#plans}}<li>{{name}}</li>{{/plans}}</ul>',
+			plan: '<div class="plan">{{name}}</div>',
 			events: '<ul class="events">{{#events}}<li>{{name}}</li>{{/events}}</ul>',
+			event: '<div class="event">{{name}}</div>',
 			account: '<div class="account"><h3>{{name}}</h3><a href="mailto:{{email}}">{{email}}</a></div>',
 			login: '<form class="login"><input name="username" type="text" /><input name="password" type="password" /><button type="submit">Log In</button></form>',
 			register: '<form class="login"><input name="email" type="text" /><input name="username" type="text" /><input name="password" type="password" /><button type="submit">Register</button></form>',
 			receipt: '<ul class="receipt">{{#items}}<li>{{name}}</li>{{/items}}</ul>',
 			orders: '<ul class="orders">{{#orders}}<li>{{contents}}</li>{{/orders}}</ul>',
+			order: '<div class="order">{{contents}}</div>',
 			errors: '<ul class="errors">{{#errors}}<li>{{message}}</li>{{/errors}}</ul>'
 		};
 		if( parameters.templates ) $.extend( templates, parameters.templates );
@@ -30,241 +34,244 @@ this.sparkart = {};
 		// comb the markup for areas we need to replace	
 		// assign new templates if necessary
 		
-		fanclub.register = function( data, callback ){
-			
-			fanclub.post( 'register', data, function( err, response ){
-				
-				if( err ){
-					if( callback ) callback( err );
-					return;
-				}
-				
-				if( callback ) callback( null, response );
-				fanclub.draw();
-				
-			});
-			
-		};
-		
-		fanclub.login = function( data, callback ){
-			
-			fanclub.post( 'login', data, function( err, response ){
-				
-				if( err ){
-					if( callback ) callback( err );
-					return;
-				}
-				
-				if( callback ) callback( null, response );
-				fanclub.draw();
-				
-			});
-			
-		};
-		
-		fanclub.logout = function( callback ){
-			
-			fanclub.post( 'logout', null, function( err, response ){
-				
-				if( err ){
-					if( callback ) callback( err );
-					return;
-				}
-				
-				if( callback ) callback( null, response );
-				fanclub.draw();
-				
-			});
-			
-		};
-		
-		// redraw widgets
-		// used when authorization status changes
-		fanclub.draw = function( $widget, config ){
-			
-			if( !$widget ){
-				var $widgets = $('.sparkart.fanclub');
-				$widgets.each( function( i, widget ){
-					fanclub.draw( widget )
-				});
-				return;
-			}
-			
-			var widget;	
-			$widget = $($widget); // make sure it's a jquery object	
-			config = config || $widget.data() || {};	
-			
-			if( $widget.is('.subscriptions') ) widget = 'subscriptions';
-			else if( $widget.is('.plans') ) widget = 'plans';
-			else if( $widget.is('.events') ) widget = 'events';
-			else if( $widget.is('.receipt') ) widget = 'receipt';
-			else if( $widget.is('.login') ) widget = 'login';
-			else if( $widget.is('.register') ) widget = 'register';
-			else if( $widget.is('.account') ) widget = 'account';
-			else if( $widget.is('.orders') ) widget = 'orders';
-			
-			$widget
-				.removeClass('error')
-				.addClass('loading');	
-			
-			fanclub.renderWidget( widget, config, function( err, html ){
-				if( err ){
-					$widget
-						.removeClass('loading')
-						.addClass('error');
-					return;
-				}
-				$widget
-					.html( html )
-					.removeClass('loading');
-			});	
-			
-		};
-		
-		// generate a widget's markup
-		fanclub.renderWidget = function( widget, config, callback ){
-			
-			if( widget === 'login' || widget === 'register' ){
-				fanclub.get( 'account', function( err, response ){
-					if( err ) return callback( err );
-					return callback( null, templates[widget]( response ) );
-				});
-			}
-			
-			fanclub.get( widget, config, function( err, response ){
-				if( err ) return callback( err );
-				callback( null, templates[widget]( response ) );
-			});
-			
-		};
-		
-		fanclub.is = function( status, callback ){
-			
-			var request;	
-			
-			if( status === 'logged in' ){
-				request = fanclub.get( 'account', function( err, response ){
-					if( err ) return callback( err );
-					var logged_in = !!response.id;
-					callback( null, logged_in );
-				});
-			}
-			
-			return request;
-			
-		};
-		
-		fanclub.request = function( url, method, parameters, callback ){
-			
-			var request = $.ajax({
-				url: url,
-				method: method,
-				dataType: 'jsonp',
-				data: parameters
-			});
-			
-			request
-				.done( function( data ){
-					if( callback ) callback( null, data );
-				})
-				.fail( function( request, error_name, err ){
-					if( callback ) callback( err );
-				});
-				
-			return request;
-			
-		};
-		
-		// shortcut for requesting api data
-		fanclub.get = function( endpoint, parameters, callback ){
-			
-			if( typeof callback === 'undefined' ){
-				callback = parameters;
-				parameters = null;
-			}
-			var url = API_URL +'/'+ endpoint;
-			
-			return fanclub.request( url, 'GET', parameters, callback );
-			
-		};
-		
-		fanclub.post = function( endpoint, parameters, callback ){
-			
-			var url = API_URL +'/'+ endpoint;
-			
-			return fanclub.request( url, 'POST', parameters, callback );
-			
-		};
-		
-		// 
-		fanclub.bindEvents = function( $widgets ){
-			
-			$('.sparkart.fanclub.login').on( 'submit.sparkart', function( e ){
-				
-				e.preventDefault();
-				
-				var $this = $(this);
-				var data = {
-					username: $this.find('input[name="username"]').val(),
-					password: $this.find('input[name="password"]').val()
-				};
-					
-				fanclub.login( data, function( err, response ){
-					
-					// remove old error message
-					var $previous_errors = $this.data('sparkart_$errors');
-					if( $previous_errors ) $previous_errors.remove();
-					
-					if( err ){
-						var $err = $( templates.errors( err ) );
-						$this.prepend( $err );
-						$this.data( 'sparkart_$errors', $err );
-						return;
-					}	
-					
-				});	
-				
-			});
-			
-			$('.sparkart.fanclub.register').on( 'submit.sparkart', function( e ){
-				
-				e.preventDefault();
-				
-				var $this = $(this);
-				var data = {
-					username: $this.find('input[name="username"]').val(),
-					email: $this.find('input[name="email"]').val(),
-					password: $this.find('input[name="password"]').val(),
-					password_confirm: $this.find('input[name="password_confirm"]').val(),
-					terms: $this.find('input[name="terms"]').val()
-				};
-				
-				fanclub.register( data, function( err, response ){
-				
-					// remove old error message
-					var $previous_errors = $this.data('sparkart_$errors');
-					if( $previous_errors ) $previous_errors.remove();
-					
-					if( err ){
-						var $err = $( templates.errors( err ) );
-						$this.prepend( $err );
-						$this.data( 'sparkart_$errors', $err );
-						return;
-					}
-					
-				});
-				
-			});
-			
-		};
-		
-		fanclub.destroy = function(){
-		
-		};
-		
 		// draw all widgets
 		fanclub.draw();
 		fanclub.bindEvents();
+		
+	};
+	
+	// register the user
+	Fanclub.prototype.register = function( data, callback ){
+		
+		this.post( 'register', data, function( err, response ){
+			
+			if( err ){
+				if( callback ) callback( err );
+				return;
+			}
+			
+			if( callback ) callback( null, response );
+			fanclub.draw();
+			
+		});
+		
+	};
+	
+	// log the user in
+	Fanclub.prototype.login = function( data, callback ){
+		
+		var fanclub = this;	
+		
+		this.post( 'login', data, function( err, response ){
+			
+			if( err ){
+				if( callback ) callback( err );
+				return;
+			}
+			
+			if( callback ) callback( null, response );
+			fanclub.draw();
+			
+		});
+		
+	};
+	
+	// log the user out
+	Fanclub.prototype.logout = function( callback ){
+		
+		var fanclub = this;	
+		
+		this.post( 'logout', null, function( err, response ){
+			
+			if( err ){
+				if( callback ) callback( err );
+				return;
+			}
+			
+			if( callback ) callback( null, response );
+			fanclub.draw();
+			
+		});
+		
+	};
+	
+	// redraw widgets
+	// used when authorization status changes
+	Fanclub.prototype.draw = function( $widget, config ){
+		
+		var fanclub = this;	
+		
+		if( !$widget ){
+			var $widgets = $('.sparkart.fanclub');
+			$widgets.each( function( i, widget ){
+				fanclub.draw( widget )
+			});
+			return;
+		}
+		
+		var widget;	
+		$widget = $($widget); // make sure it's a jquery object	
+		config = config || $widget.data() || {};	
+		
+		if( $widget.is('.subscriptions') ) widget = 'subscriptions';
+		else if( $widget.is('.plans') ) widget = 'plans';
+		else if( $widget.is('.events') ) widget = 'events';
+		else if( $widget.is('.receipt') ) widget = 'receipt';
+		else if( $widget.is('.login') ) widget = 'login';
+		else if( $widget.is('.register') ) widget = 'register';
+		else if( $widget.is('.account') ) widget = 'account';
+		else if( $widget.is('.orders') ) widget = 'orders';
+		
+		$widget
+			.removeClass('error')
+			.addClass('loading');	
+		
+		this.renderWidget( widget, config, function( err, html ){
+			if( err ){
+				$widget
+					.removeClass('loading')
+					.addClass('error');
+				return;
+			}
+			$widget
+				.html( html )
+				.removeClass('loading');
+		});	
+		
+	};
+	
+	// generate a widget's markup
+	Fanclub.prototype.renderWidget = function( widget, config, callback ){
+		
+		if( widget === 'login' || widget === 'register' ){
+			this.get( 'account', function( err, response ){
+				if( err ) return callback( err );
+				return callback( null, templates[widget]( response ) );
+			});
+		}
+		
+		this.get( widget, config, function( err, response ){
+			if( err ) return callback( err );
+			callback( null, templates[widget]( response ) );
+		});
+		
+	};
+	
+	// general method for doing AJAX requests
+	Fanclub.prototype.request = function( url, method, parameters, callback ){
+		
+		var request = $.ajax({
+			url: url,
+			method: method,
+			dataType: 'jsonp',
+			data: parameters
+		});
+		
+		request
+			.done( function( data ){
+				if( callback ) callback( null, data );
+			})
+			.fail( function( request, error_name, err ){
+				if( callback ) callback( err );
+			});
+		
+		return request;
+		
+	};
+	
+	// shortcut for requesting api data
+	Fanclub.prototype.get = function( endpoint, parameters, callback ){
+		
+		if( typeof callback === 'undefined' && typeof parameters === 'function' ){
+			callback = parameters;
+			parameters = null;
+		}
+		var url = API_URL +'/'+ endpoint;
+		
+		return this.request( url, 'GET', parameters, callback );
+		
+	};
+	
+	// shortcut for sending api data
+	Fanclub.prototype.post = function( endpoint, parameters, callback ){
+		
+		if( typeof callback === 'undefined' && typeof parameters === 'function' ){
+			callback = parameters;
+			parameters = null;
+		}
+		var url = API_URL +'/'+ endpoint;
+		
+		return this.request( url, 'POST', parameters, callback );
+		
+	};
+	
+	// bind DOM events to widgets
+	Fanclub.prototype.bindEvents = function( $widgets ){
+		
+		var fanclub = this;	
+		
+		// bind all login widgets
+		$('.sparkart.fanclub.login').on( 'submit.sparkart', function( e ){
+			
+			e.preventDefault();
+			
+			var $this = $(this);
+			var data = {
+				username: $this.find('input[name="username"]').val(),
+				password: $this.find('input[name="password"]').val()
+			};
+			
+			fanclub.login( data, function( err, response ){
+				
+				// remove old error message
+				var $previous_errors = $this.data('sparkart_$errors');
+				if( $previous_errors ) $previous_errors.remove();
+				
+				if( err ){
+					var $err = $( templates.errors( err ) );
+					$this.prepend( $err );
+					$this.data( 'sparkart_$errors', $err );
+					return;
+				}	
+				
+			});	
+			
+		});
+		
+		// bind all register widgets
+		$('.sparkart.fanclub.register').on( 'submit.sparkart', function( e ){
+			
+			e.preventDefault();
+			
+			var $this = $(this);
+			var data = {
+				username: $this.find('input[name="username"]').val(),
+				email: $this.find('input[name="email"]').val(),
+				password: $this.find('input[name="password"]').val(),
+				password_confirm: $this.find('input[name="password_confirm"]').val(),
+				terms: $this.find('input[name="terms"]').val()
+			};
+			
+			fanclub.register( data, function( err, response ){
+				
+				// remove old error message
+				var $previous_errors = $this.data('sparkart_$errors');
+				if( $previous_errors ) $previous_errors.remove();
+				
+				if( err ){
+					var $err = $( templates.errors( err ) );
+					$this.prepend( $err );
+					$this.data( 'sparkart_$errors', $err );
+					return;
+				}
+				
+			});
+			
+		});
+		
+	};
+	
+	Fanclub.prototype.destroy = function(){
 		
 	};
 	
