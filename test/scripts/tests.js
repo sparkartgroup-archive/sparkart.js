f1ee4dcc4e9bbc1c15f3c3d0f9c058f9613bb615
Fauntleroy <the.kaempfer@gmail.com>
2013-01-09 17:38:24 -0800

    add basic test structure
    add methods for mocking AJAX

--- test/scripts/tests.js
+++ test/scripts/tests.js
@@ -1,5 +1,114 @@
-describe( 'Jasmine', function(){
-	it( 'makes testing JavaScript awesome!', function(){
-		expect( true ).toBe( true );
+describe( 'Fanclub', function(){
+
+	it( 'draws all widgets found on the page', function(){
 	});
+
+	it( 'creates a parameters object from defaults and specified options', function(){
+	});
+
+	it( 'gets initial fanclub data', function(){
+	});
+
+	describe( 'register', function(){
+
+		it( 'registers a user', function(){
+		});
+
+	});
+
+	describe( 'login', function(){
+
+		it( 'logs a user in', function(){
+		});
+
+	});
+
+	describe( 'logout', function(){
+
+		it( 'logs a user out', function(){
+		});
+
+	});
+
+	describe( 'draw', function(){
+
+		it( 'draws every widget on the page when none are specified', function(){
+		});
+
+		it( 'draws the specified widget', function(){
+		});
+
+	});
+
+	describe( 'renderWidget', function(){
+
+		it( 'provides the markup of a widget', function(){
+		});
+
+	});
+
+	describe( 'request', function(){
+
+		it( 'requests data from the API', function(){
+		});
+
+	});
+
+	describe( 'get', function(){
+
+		it( 'initiates a GET request', function(){
+		});
+
+	});
+
+	describe( 'post', function(){
+
+		it( 'initiates a POST request', function(){
+		});
+
+	});
+
+	describe( 'bindWidget', function(){
+
+		it( 'binds events to a login widget', function(){
+		});
+
+		it( 'binds events to a register widget', function(){
+		});
+
+		it( 'binds events to a logout widget', function(){
+		});
+
+		it( 'binds events to an account widget', function(){
+		});
+
+		it( 'binds events to a password reset widget', function(){
+		});
+
+	});
+
+	describe( 'on', function(){
+
+		it( 'binds a single event handler', function(){
+		});
+
+	});
+
+	describe( 'trigger', function(){
+
+		it( 'triggers all event handlers for the specified event', function(){
+		});
+
+	});
+
+	describe( 'off', function(){
+
+		it( 'removes a single event handler from the specified event', function(){
+		});
+
+		it( 'removes all event handlers from the specified event', function(){
+		});
+
+	});
+
 });
