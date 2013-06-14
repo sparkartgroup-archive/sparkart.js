3652a65a8ae5f388fc1e363cd31eecbcc97dce97
Eric Lanehart <pushred@gmail.com>
2013-06-13 17:06:03 -0700

    Increment version to 000.004.001

--- sparkart.js
+++ sparkart.js
@@ -1,5 +1,5 @@
-/* Sparkart.js v000.003.002
-   Generated on 2013-06-13 at 12:49:21 */
+/* Sparkart.js v000.004.001
+   Generated on 2013-06-13 at 17:05:37 */
 
 // Add sparkart to the global namespace
 this.sparkart = {};
